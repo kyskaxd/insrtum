a3ef9f50d77b2aa3509c57b25449146a7f9c71b6
Arseniy <senakoz777@gmail.com>
2025-10-30 09:17:39 +0400

    final ver

--- test.py
+++ test.py
@@ -1,26 +1,28 @@
 def func(x):
     return 5 / (3*x - x*x - 3)
 
-
-x_start = float(input("Введите начальную точку отрезка x: "))
+x_start = float(input("Введите начальную точку: "))
+x_end = float(input("Введите конечную точку: "))
 h = float(input("Введите шаг h (>0): "))
 
 while h <= 0:
-    print("Шаг h должен быть положительным!")
+    print("Шаг h должен быть положительным")
     h = float(input("Введите шаг h (>0): "))
 
-x_end = x_start + h
-
 current_x = x_start
-min_x = current_x
-min_y = func(current_x)
+x_next = current_x + h
+y_next = func(x_next)
+min_x = x_start
+min_y = func(x_start)
+
 
 while current_x <= x_end:
-    y = func(current_x)
-    if y < min_y:
-        min_y = y
-        min_x = current_x
+    current_y = func(current_x)
+    
+    if current_y < y_next:
+        if current_y  < min_y:
+            min_y = current_y 
+            min_x = current_x
     current_x += h
 
-print(f"Локальный минимум функции на отрезке [{x_start}, {x_end}]:")
 print(f"x = {round(min_x, 3)}, y = {round(min_y, 3)}")
